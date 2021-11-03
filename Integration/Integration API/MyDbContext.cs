625c545288c0d3597f70742cbb9aca143a442ce5
vujsa7 <61715001+vujsa7@users.noreply.github.com>
2021-11-03 15:41:12 +0100

    Changed structure of project setup (#1)

---
+++ Integration/Integration API/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Integration_API.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
