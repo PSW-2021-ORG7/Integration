8ff19611ab3ba75566321d9dcb89336d479f92b6
DejanBjelic <dejan.bjelic@uns.ac.rs>
2021-11-06 14:01:06 +0100

    feat: Implementation of database for storing feedback (response) received from pharmacies

---
+++ Integration/Integration Class Library/Migrations/FeedbackDb/20211106125357_FeedbackMigration2.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Integration_Class_Library.Migrations.FeedbackDb
+{
+    public partial class FeedbackMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Feedbacks",
+                table: "Feedbacks");
+
+            migrationBuilder.RenameTable(
+                name: "Feedbacks",
+                newName: "Feedback");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Feedback",
+                table: "Feedback",
+                column: "IdFeedback");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Feedback",
+                table: "Feedback");
+
+            migrationBuilder.RenameTable(
+                name: "Feedback",
+                newName: "Feedbacks");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Feedbacks",
+                table: "Feedbacks",
+                column: "IdFeedback");
+        }
+    }
+}
