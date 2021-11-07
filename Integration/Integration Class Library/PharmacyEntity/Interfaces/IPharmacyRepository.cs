c30aeb3fe2aaff32549a3675ec43fe229ff48092
DejanBjelic <dejan.bjelic@uns.ac.rs>
2021-11-07 21:39:04 +0100

    refactor: Changing names and annotations

--- Integration/Integration Class Library/PharmacyEntity/Interfaces/IPharmacyRepository.cs
+++ Integration/Integration Class Library/PharmacyEntity/Interfaces/IPharmacyRepository.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Integration_Class_Library.PharmacyEntity.Interfaces
--- Integration/Integration Class Library/PharmacyEntity/Models/Pharmacy.cs
+++ Integration/Integration Class Library/PharmacyEntity/Models/Pharmacy.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
 namespace Integration_Class_Library.Models
 {
