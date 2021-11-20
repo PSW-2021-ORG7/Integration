853026559a495d06443d5733d93414e66662fafd
Marija Klještan <marijakljestan@uns.ac.rs>
2021-11-20 02:42:05 +0100

    refactor: Project updated, Test controller

---
+++ Integration/Integration API/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Integration_Class_Library.SharedModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Integration_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok(new TestClass().Test());
+        }
+    }
+}
--- Integration/Integration Class Library/SharedModel/TestClass.cs
+++ Integration/Integration Class Library/SharedModel/TestClass.cs
@@ -1,6 +1,10 @@
-﻿namespace Integration_Class_Library.Model
+﻿namespace Integration_Class_Library.SharedModel
 {
     public class TestClass
     {
+        public string Test()
+        {
+            return "It works!";
+        }
     }
 }
