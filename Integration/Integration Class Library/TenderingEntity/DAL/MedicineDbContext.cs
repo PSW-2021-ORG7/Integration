5a53821f983c8e3ef3bb2c5746b30d093104955e
Dejan Bjelic <iodum99@yahoo.com>
2021-11-24 01:47:28 +0100

    feat: Added medicine model and dbcontext

---
+++ Integration/Integration Class Library/TenderingEntity/DAL/MedicineDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Integration_Class_Library.TenderingEntity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Integration_Class_Library.TenderingEntity.DAL
+{
+    class MedicineDbContext : DbContext
+    {
+        public MedicineDbContext(DbContextOptions<MedicineDbContext> options) : base(options) { }
+
+        public DbSet<Medicine> Medicine { get; set; }
+        public DbSet<MedicineInventory> MedicineInventory { get; set; }
+        public DbSet<Ingredient> Ingredient { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            builder.Entity<Ingredient>()
+                .HasIndex(i => i.Name)
+                .IsUnique();
+        }
+    }
+}
+
