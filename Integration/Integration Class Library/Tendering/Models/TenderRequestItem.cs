c00d33dcc2a9d27518af29b6f0e9d82b7d0a52c6
Dejan Bjelic <iodum99@yahoo.com>
2022-01-09 01:31:10 +0100

    refactor: Changed request object type

---
+++ Integration/Integration Class Library/Tendering/Models/TenderRequestItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Integration_Class_Library.Tendering.Models
+{
+    public class TenderRequestItem
+    {
+        public String MedicineName { get; set; }
+        public int DosageInMilligrams { get; set; }
+        public String Manufacturer { get; set; }
+        public int RequiredQuantity { get; set; }
+
+        public TenderRequestItem() { }
+    }
+}
